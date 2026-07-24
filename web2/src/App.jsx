d84d17b39f27a5e1f61717b923fabfb7207da765
Ozodbek Xayitboyev <xayitboyev.dev@gmail.com>
2026-07-24 19:49:13 +0500

    feat: [web2] ready

--- web2/src/App.jsx
+++ web2/src/App.jsx
@@ -5,8 +5,8 @@ import axios from "axios";
 import "./App.css";
 
 const BOT_TOKEN = "8925060597:AAHCm35W3slmpxlGmI2xmTTgSRApFj5LMes";
-const CHAT_ID = "-1004307112486";
-const TARGET_DATE = "2026-07-05 21:00:00";
+const CHAT_ID = "-1004330768512";
+const TARGET_DATE = "2026-08-05 20:00:00";
 
 export default function App() {
   const [loading, setLoading] = useState();
@@ -72,7 +72,7 @@ export default function App() {
       e.target.reset();
 
       setTimeout(() => {
-        window.location.href = "https://t.me/kuhisim";
+        window.location.href = "https://t.me/baraka_premiumm";
       }, 500);
     } catch (error) {
       toast.error(error.response?.data || error.message);
@@ -86,26 +86,28 @@ export default function App() {
     <div className='min-h-screen'>
       <div className="flex flex-col gap-4 md:max-w-md mx-auto">
         {/* Header */}
-        <div className="flex items-center justify-between p-6">
+        <div className="flex items-center justify-between p-6 mb-[-18px]" style={{ background: "#93b8de" }}>
           <div className="flex items-center gap-2 py-2 px-3 rounded-xl bg-white">
             <img src="/calendar.png" className="w-5 h-5" />
-            <span className="font-semibold">5-Iyul</span>
+            <span className="font-semibold">5-Avgust</span>
           </div>
           <img src="/logo.png" className="w-22" />
-          <div className="flex items-center gap-2 py-2 px-3 rounded-xl bg-white">
+          <div className="flex items-center gap-2 py-2 px-5 rounded-xl bg-white">
             <img src="/clock.png" className="w-5 h-5" />
-            <span className="font-semibold">21:00</span>
+            <span className="font-semibold">20:00</span>
           </div>
         </div>
 
         {/* Hero */}
-        <div className="flex flex-col mb-[-50px]">
-          <h2 className="roboto-condensed-bold text-center text-3xl font-bold">ANGREN SHAHRIDA</h2>
-          <h2 className="roboto-condensed-bold text-center text-4xl text-orange-500 font-bold">MAXSUS TAKLIFLARDA</h2>
-          <h2 className="roboto-condensed-bold text-center text-3xl font-bold">XONADON XARID QILING</h2>
+        <div className="flex flex-col " style={{ background: "linear-gradient(to bottom, #93b8de, #93b8de, #93b8de, transparent)" }}>
+          {/* <h2 className="roboto-condensed-bold text-center text-3xl font-bold text-white">OLMALIQ SHAHRIDA</h2>
+          <h2 className="roboto-condensed-bold text-center text-4xl text-[var(--primary)] font-bold bg-gradient-to-b from-orange-500 via-yellow-500 to-orange-500 bg-clip-text">3 900 000 so‘mdan</h2>
+          <h2 className="roboto-condensed-bold text-center text-3xl font-bold text-white">XONADON XARID QILING</h2> */}
 
-          <div className="relative overflow-hidden translate-y-[-50px]">
-            <img src="/hero.png" className="w-full translate-y-[50px]" />
+          <img src="/heroText.png" className="translate-y-12 z-10" />
+
+          <div className="relative">
+            <img src="/hero2.png" className="" />
             <a href="https://t.me/kuhisim" className="absolute text-white text-2xl w-[84%] font-bold bottom-0 left-[50%] translate-x-[-50%] flex items-center justify-center gap-4 bg-orange-500 rounded-full py-5">
               <img src="/telegram.png" className="w-7" />
               TELEGRAM KANAL
@@ -120,16 +122,43 @@ export default function App() {
           <h4>ULGURIB QOLING</h4>
         </div>
 
+        {/* Section counter */}
+        <div className="flex flex-col gap-4 items-center bg-white rounded-4xl p-4 m-3 mt-0">
+          <h2 className="text-[var(--primary)] font-bold text-xl">AKSIYAGACHA QOLDI</h2>
+
+          <div className="flex items-center gap-1 w-full text-2xl font-semibold">
+            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-[var(--primary)] rounded-3xl py-2">
+              <h2 className="text-4xl font-bold">{String(time.days).padStart(2, "0")}</h2>
+              <span className="text-gray-500 text-sm font-medium">KUN</span>
+            </div>
+            <span>:</span>
+            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-[var(--primary)] rounded-3xl py-2">
+              <h2 className="text-4xl font-bold">{String(time.hours).padStart(2, "0")}</h2>
+              <span className="text-gray-500 text-sm font-medium">SOAT</span>
+            </div>
+            <span>:</span>
+            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-[var(--primary)] rounded-3xl py-2">
+              <h2 className="text-4xl font-bold">{String(time.minutes).padStart(2, "0")}</h2>
+              <span className="text-gray-500 text-sm font-medium">DAQIQA</span>
+            </div>
+            <span>:</span>
+            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-[var(--primary)] rounded-3xl py-2">
+              <h2 className="text-4xl font-bold">{String(time.seconds).padStart(2, "0")}</h2>
+              <span className="text-gray-500 text-sm font-medium">SONIYA</span>
+            </div>
+          </div>
+        </div>
+
         {/* Section gifts */}
         <div className="flex flex-col gap-4 items-center bg-white rounded-4xl m-3 mt-0 p-2">
           <div className="flex items-center gap-2 mt-2">
-            <h2 className="text-orange-500 font-bold text-xl">MAXSUS SOVG'ALAR</h2>
+            <h2 className="text-[var(--primary)] font-bold text-xl">MAXSUS SOVG'ALAR</h2>
             <img src="/gift.png" className="w-7" />
           </div>
           <div className="grid grid-cols-3 gap-2 w-full">
             <div className="flex flex-col gap-2 items-center py-2 bg-gray-100 rounded-3xl">
-              <img src="/gifts/home.png" className="w-18 h-18" />
-              <h4 className="text-gray-600 text-xs">1 XONALI UY</h4>
+              <img src="/gifts/onix.png" className="w-25 h-18" />
+              <h4 className="text-gray-600 text-xs">ONIX MASHINASI</h4>
             </div>
             <div className="flex flex-col gap-2 items-center py-2 bg-gray-100 rounded-3xl">
               <img src="/gifts/tv.png" className="w-18 h-18" />
@@ -154,37 +183,10 @@ export default function App() {
           </div>
         </div>
 
-        {/* Section counter */}
-        <div className="flex flex-col gap-4 items-center bg-white rounded-4xl p-4 m-3 mt-0">
-          <h2 className="text-orange-500 font-bold text-xl">AKSIYAGACHA QOLDI</h2>
-
-          <div className="flex items-center gap-1 w-full text-2xl font-semibold">
-            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-orange-500 rounded-3xl py-2">
-              <h2 className="text-4xl font-bold">{String(time.days).padStart(2, "0")}</h2>
-              <span className="text-gray-500 text-sm font-medium">KUN</span>
-            </div>
-            <span>:</span>
-            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-orange-500 rounded-3xl py-2">
-              <h2 className="text-4xl font-bold">{String(time.hours).padStart(2, "0")}</h2>
-              <span className="text-gray-500 text-sm font-medium">SOAT</span>
-            </div>
-            <span>:</span>
-            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-orange-500 rounded-3xl py-2">
-              <h2 className="text-4xl font-bold">{String(time.minutes).padStart(2, "0")}</h2>
-              <span className="text-gray-500 text-sm font-medium">DAQIQA</span>
-            </div>
-            <span>:</span>
-            <div className="flex flex-col gap-1 items-center flex-1 border-1 border-orange-500 rounded-3xl py-2">
-              <h2 className="text-4xl font-bold">{String(time.seconds).padStart(2, "0")}</h2>
-              <span className="text-gray-500 text-sm font-medium">SONIYA</span>
-            </div>
-          </div>
-        </div>
-
-        {/* Section gifts */}
+        {/* Section waiting */}
         <div className="flex flex-col gap-4 items-center bg-white rounded-4xl m-3 mt-0 p-2">
           <div className="flex items-center gap-2 mt-2">
-            <h2 className="text-orange-500 font-bold text-xl">MAXSUS SOVG'ALAR</h2>
+            <h2 className="text-[var(--primary)] font-bold text-xl">SIZNI KUTMOQDA</h2>
             <img src="/gift.png" className="w-7" />
           </div>
           <div className="grid grid-cols-3 gap-2 w-full">
@@ -216,7 +218,7 @@ export default function App() {
         </div>
 
         {/* Footer */}
-        <a href="https://t.me/kuhisim" className="text-white text-2xl mx-3 mt-0 mb-6 font-bold flex items-center justify-center gap-4 bg-orange-500 rounded-full py-5">
+        <a href="https://t.me/baraka_premiumm" className="text-white text-2xl mx-3 mt-0 mb-6 font-bold flex items-center justify-center gap-4 bg-[var(--primary)] rounded-full py-5">
           <img src="/telegram.png" className="w-7" />
           TELEGRAM KANAL
         </a>
